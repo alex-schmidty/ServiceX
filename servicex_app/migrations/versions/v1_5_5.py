eee84facba584163e4f16d56d85bafc19a3befcb
Ben Galewsky <ben@peartreestudio.net>
2024-11-13 12:25:24 -0600

    Make db migration more robust

--- servicex_app/migrations/versions/v1_5_5.py
+++ servicex_app/migrations/versions/v1_5_5.py
@@ -23,9 +23,9 @@ def upgrade():
                                         server_default='false'))
 
     # Name is no longer unique, so we need to drop the unique index
+    op.drop_constraint('datasets_name_key', 'datasets')
     # op.drop_index('datasets_name_key', table_name='datasets')
 
-    op.drop_constraint('datasets_name_key', 'datasets')
     # This was never used, so a fine time to delete it
     op.drop_column('transform_result', 'did')
 
@@ -33,4 +33,5 @@ def upgrade():
 def downgrade():
     op.create_unique_constraint('datasets_name_key', 'datasets', ['name'])
     op.drop_column('datasets', 'stale')
-    # No need to add the transform_result.did column back in
+
+    op.add_column('transform_result', sa.Column('did', sa.String(), nullable=True))
