1e0c2965691df00144489c6d5caa578a4720d770
alexancs <alexcschmidt17@gmail.com>
2024-12-05 14:44:39 -0600

    changed transform_single_file.py to use output_path

--- code_generator_TopCPToolkit/servicex/templates/transform_single_file.py
+++ code_generator_TopCPToolkit/servicex/templates/transform_single_file.py
@@ -5,6 +5,7 @@ import subprocess
 import generated_transformer
 instance = os.environ.get('INSTANCE_NAME', 'Unknown')
 from pathlib import Path
+import shutil
 
 def transform_single_file(file_path: str, output_path: Path, output_format: str):
     # create input.txt file for event loop and insert file_path as only line
@@ -14,6 +15,7 @@ def transform_single_file(file_path: str, output_path: Path, output_format: str)
     generated_transformer.make_yaml()
 
     subprocess.run(["runTop_el.py", "-i", "input.txt", "-o", "output", "-t", "customConfig", "-e", "1"])
+    shutil.move("output.root", output_path)
 
 if __name__ == "__main__":
     transform_single_file(sys.argv[1], Path(sys.argv[2]), sys.argv[3])
